27498c2723c151abf4ea026eec5e571cf1983aaf
LuisCastroV <>
2022-04-22 16:05:16 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -80,6 +80,10 @@ Pair * searchTreeMap(TreeMap * tree, void* key)
 {
  
   TreeNode* aux=tree->root;
+  while(aux!=NULL)
+  {
+      if(is_equal(tree, key, aux->key))
+  }
   //int resultado = map->lower_than(aux->key,aux);
   // while(is_equal(tree,aux->key,key) == 0)
   // {

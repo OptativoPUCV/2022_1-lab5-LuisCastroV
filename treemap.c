23d955291eb1d9df87fdbef6402b1c87d10807db
LuisCastroV <>
2022-04-23 19:20:58 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -118,7 +118,7 @@ Pair * upperBound(TreeMap * tree, void* key) {
 }
 
 Pair * firstTreeMap(TreeMap * tree) {
-  TreeNode* node=minimum(tree->current);
+  TreeNode* node=minimum(tree->root);
   return node->value;
 }
 

feb1fad9b53f3127f1f727e1aac8f598f72b3b3c
LuisCastroV <>
2022-04-23 19:32:39 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -51,7 +51,7 @@ void insertTreeMap(TreeMap * tree, void* key, void * value) {
     TreeNode* aux=tree->current;
     TreeNode* nodo=createTreeNode(key,value);
     nodo->parent=aux;
-    printf("%d",*(int*)pair->key);
+    printf("%d",*(int*)key);
     //printf("%d",*(int*)nodo->pair->key);
     if(tree->lower_than(key,nodo->pair->key) == 1)
     {

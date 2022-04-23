697f5692c53d636166fd18c10297d7cdb6bf5221
LuisCastroV <>
2022-04-23 16:47:29 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -48,11 +48,12 @@ void insertTreeMap(TreeMap * tree, void* key, void * value) {
   Pair *dato=searchTreeMap(tree,key);
   if(dato==NULL)
   {
-    printf("JDAKWW");
-    // TreeNode* aux=tree->current;
-    // TreeNode* node=createTreeNode(key,value);
-    // node->parent=aux;
-    // //if(tree->lower_than(aux->pair->key,key)==1)
+    //printf("JDAKWW");
+    TreeNode* aux=tree->current;
+    TreeNode* node=createTreeNode(key,value);
+    node->parent=aux;
+    printf(node->parent)
+    //if(tree->lower_than(aux->pair->key,key)==1)
     // if(tree->lower_than(key,aux->key)== 1)
     // {
     //   aux->left=node;

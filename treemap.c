5c9d2c9076125f281e0998ec300f3ad532e0cae0
LuisCastroV <>
2022-04-24 21:27:57 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -114,6 +114,10 @@ void removeNode(TreeMap * tree, TreeNode* node)
       tree->current=NULL;
     }
   }
+  else if(node->right==NULL || node->left==NULL)
+  {
+    break;
+  }
 }
 
 void eraseTreeMap(TreeMap * tree, void* key){

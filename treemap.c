a18072eb4b635a4602b8b4f517bf13f4ad2d3c8d
LuisCastroV <>
2022-04-24 03:56:45 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -67,6 +67,7 @@ void insertTreeMap(TreeMap * tree, void* key, void * value)
           tree->current=tree->current->right;
         }
         tree->current=tree->current->right;
+        return;
       }
       else
       {
